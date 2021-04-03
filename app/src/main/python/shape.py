98aaa9f93eb284d79719efb42f1071c5170ce3c7
kintipu <74551789+kintipu@users.noreply.github.com>
2021-04-04 00:45:41 +0200

    Update shape.py
    Received bytearray instead of string

--- app/src/main/python/shape.py
+++ app/src/main/python/shape.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
-import base64
 
-def main(imgString):
-    decoded_data = base64.b64decode(imgString)
-    np_data = np.fromstring(decoded_data,np.uint8)
+
+def main(imageBytes):
+    np_data = np.asarray(imageBytes,np.uint8)
     img = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
+
 
     if img.shape[0] > img.shape[1]:
         img = cv2.transpose(img)
+       
+    img = cv2.resize(img, (1600, 1200))
     
     return str(str(img.shape[0]) + "_"  + str(img.shape[1])) 
